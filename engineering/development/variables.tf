a70099f0f3b2d89d4ede76dcdf8b045da5d64334
VENKATESH <vchary78@gmail.com>
2023-08-01 00:06:37 +0530

    Update variables.tf

--- engineering/development/variables.tf
+++ engineering/development/variables.tf
@@ -1,1 +1,15 @@
+variable "dev_rbac_group" {
+  type        = string
+  description = "The deploper rbac group"
+}
 
+variable "viewer_user_rbac_group" {
+  type        = string
+  description = "The viewer rbac group"
+}
+
+variable "main_location" {
+  type        = string
+  description = "The main region for deployment"
+  default     = "us-central1"
+}
