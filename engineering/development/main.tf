fd0fe72e42f23f74cfa57b9291daab5bb6f6167d
VENKATESH <vchary78@gmail.com>
2023-08-01 00:02:12 +0530

    Update main.tf

--- engineering/development/main.tf
+++ engineering/development/main.tf
@@ -1,1 +1,59 @@
+locals {
+  datasets = [
+    module.bq_data_set
+  ]
+  datasets_map = { for dataset in local.datasets: dataset.bigquery_dataset.dataset_id => dataset.bigquery_dataset }
 
+
+/*************************************
+ Project id's
+**************************************/
+
+
+data "google_project" "test_bq_prj" {
+  project_id = "xxx-xx-xx-125vgd"
+}
+
+/*************************************
+Dataset creation
+**************************************/
+
+module "test_bq_data_set" {
+  source               = "terraform-google-modules/bigquery/google"
+  version              = "~> 5.4.2"
+  dataset_id           = "test_dataset_id"
+  dataset_name         = "test_dataset"
+  description          = "A test data set contains users data"
+  project_id           = data.google_project.test_bq_prj.project_id
+  location             = var.main_location
+  access               = [] # empty for access
+}
+
+
+/*************************************
+ IAM Permission
+ **************************************/
+
+module "test_bq_data_set_access" {
+  source               = "terraform-google-modules/bigquery/google//modules/authorization"
+  version              = "~> 5.4.2"
+
+  dataset_id           = module.test_bq_data_set.bigquery_dataset.dataset_id
+  project_id           = data.google_project.test_bq_prj.project_id
+
+  roles = concat([
+    {
+      role             = "roles/bigquery.dataViewer"
+      group_by_email   = var.viewer_user_rbac_group
+    }
+  ],
+  [
+    {
+        role           = "roles/bigquery.dataOwner"
+        group_by_email = var.dev_rbac_group
+      }
+    ]
+
+  )
+  authorized_views     = []
+}
